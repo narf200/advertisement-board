ddbc9afe7174d1ca903baae6ccdd1b84772d0fe9
narf200 <narf200@gmail.com>
2021-07-15 00:48:13 +0500

    edited by reed mi

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -90,13 +90,13 @@ function NoteCard({card}) {
     console.log(timestamp)
 
     const formatDate = (timestamp) => {
-       let date = new Date(timestamp * 1000)
-        let year = date.getFullYear();
-        let month = ('0' + (date.getMonth() + 1))
-        let day = ( '0' + (date.getDay() + 1));
-        let hours = date.getHours();
-        let minutes = "0" + date.getMinutes();
-        let seconds = "0" + date.getSeconds();
+       const date = new Date(timestamp * 1000)
+       const year = date.getFullYear();
+       const month = ('0' + (date.getMonth() + 1))
+       const day = ( '0' + (date.getDay() + 1));
+       const hours = date.getHours();
+       const minutes = "0" + date.getMinutes();
+       const seconds = "0" + date.getSeconds();
        return  `${year} ${month} ${day} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`
     }
 
